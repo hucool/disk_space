3cb7b56e33b5220e421c691221011bb3b55d2233
Tiger <hucool@users.noreply.github.com>
2022-04-08 10:07:22 +0800

    Update DiskSpacePlugin.m
    fix build err

--- ios/Classes/DiskSpacePlugin.m
+++ ios/Classes/DiskSpacePlugin.m
@@ -1,5 +1,12 @@
 #import "DiskSpacePlugin.h"
+#if __has_include(<disk_space/disk_space-Swift.h>)
 #import <disk_space/disk_space-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "disk_space-Swift.h"
+#endif
 
 @implementation DiskSpacePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
